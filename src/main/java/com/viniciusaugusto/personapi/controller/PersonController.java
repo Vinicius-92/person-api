b106c6ee2c990726b8833b583eb77a5d1284974c
Vinicius Augusto <vinicius_augusto_@msn.com>
2021-04-19 23:34:19 -0300

    Added personcontroller, next: add database and finish html

---
+++ src/main/java/com/viniciusaugusto/personapi/controller/PersonController.java
@@ -0,0 +1,48 @@
+package com.viniciusaugusto.personapi.controller;
+
+import com.viniciusaugusto.personapi.dto.request.PersonDTO;
+import com.viniciusaugusto.personapi.dto.response.MessageResponseDTO;
+import com.viniciusaugusto.personapi.exception.PersonNotFoundException;
+import com.viniciusaugusto.personapi.service.PersonService;
+import lombok.AllArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/v1/people")
+@AllArgsConstructor(onConstructor = @__(@Autowired))
+public class PersonController {
+
+    private PersonService service;
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public MessageResponseDTO createPerson(@RequestBody @Valid PersonDTO dto) {
+        return service.createPerson(dto);
+    }
+
+    @GetMapping
+    public List<PersonDTO> listAll() {
+        return service.listAll();
+    }
+
+    @GetMapping("/{id}")
+    public PersonDTO findById(@PathVariable Long id) throws PersonNotFoundException {
+        return service.findById(id);
+    }
+
+    @PutMapping("/{id}")
+    public MessageResponseDTO updateById(@PathVariable Long id, @RequestBody @Valid PersonDTO dto) throws PersonNotFoundException {
+        return service.updateById(id, dto);
+    }
+
+    @DeleteMapping("/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void deleteById(@PathVariable Long id) throws PersonNotFoundException {
+        service.delete(id);
+    }
+}
