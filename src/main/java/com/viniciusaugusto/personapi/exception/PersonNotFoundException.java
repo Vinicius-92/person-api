98e0e993ea313955e34ccfba8a29bddf0c8d01cb
Vinicius Augusto <vinicius_augusto_@msn.com>
2021-04-19 23:01:54 -0300

    Added HTML placeholder and Person Service

---
+++ src/main/java/com/viniciusaugusto/personapi/exception/PersonNotFoundException.java
@@ -0,0 +1,12 @@
+package com.viniciusaugusto.personapi.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.BAD_REQUEST)
+public class PersonNotFoundException extends Exception{
+
+    public PersonNotFoundException(Long id){
+        super("Person not found with ID" + id);
+    }
+}
